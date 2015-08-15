7ace17ecc04626c95cf2e39bc34fee189f9c3b92
jimmyzywang <jimmyzywang@qq.com>
2015-08-15 15:42:58 +0800

    Initial Commit

---
+++ OneBitBabyUrine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OneBitBabyUrine
+//
+//  Created by jimmyzywang-nb on 15/8/15.
+//  Copyright (c) 2015年 com.tencent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
